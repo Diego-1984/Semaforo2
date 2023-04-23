e8dc5dbd24bcbaac3926d670c477020f8e939827
Diego-1984 <diegomenoruceta@gmail.com>
2023-04-23 11:57:40 +0000

    Proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const {selectedColor, setSelectecColor }= useState("")
-		return (
+	const [selectedColor, setSelectedColor] = useState(""); // Corregir: la función useState debe devolver una matriz con dos elementos
+	
+	return (
 		<>
-		<div className="palito"></div>
-		<div className="semaforo">
-			<div
-			onClick={()=>setSelectecColor("red")} 
-			className={"light red" + ((selectedColor === "red") ? " glow": "")}>
+			<div className="palito"></div>
+			<div className="semaforo">
+				<div
+				onClick={()=>setSelectedColor("red")} // Corregir: cambiar "setSelectecColor" a "setSelectedColor"
+				className={"light red" + ((selectedColor === "red") ? " glow": "")}>
+
+				</div>
+				<div
+				onClick={()=>setSelectedColor("yellow")} 
+				className={"light yellow" + ((selectedColor === "yellow") ? " glow": "")}>
+
+				</div>
+				<div
+				onClick={()=>setSelectedColor("green")} 
+				className={"light green" + ((selectedColor === "green") ? " glow": "")}>
+
+				</div>
 
 			</div>
-			<div
-			onClick={()=>setSelectecColor("yellow")} 
-			className={"light yellow" + ((selectedColor === "yellow") ? " glow": "")}>
-
-			</div>
-			<div
-			onClick={()=>setSelectecColor("green")} 
-			className={"light green" + ((selectedColor === "green") ? " glow": "")}>
-
-			</div>
-			
-		</div>
 		</>	
 	);
 };
