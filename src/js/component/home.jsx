02805647371cfaa2fc9fafcbd13aaaebad23ce4a
Diego-1984 <diegomenoruceta@gmail.com>
2025-01-16 11:58:16 +0000

    Versión 3.0 finalizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,35 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState(""); // Corregir: la función useState debe devolver una matriz con dos elementos
-	
-	return (
-		<>
-			<div className="palito"></div>
-			<div className="semaforo">
-				<div
-				onClick={()=>setSelectedColor("red")} // Corregir: cambiar "setSelectecColor" a "setSelectedColor"
-				className={"light red" + ((selectedColor === "red") ? " glow": "")}>
+  const [selectedColor, setSelectedColor] = useState(""); // Corregir: la función useState debe devolver una matriz con dos elementos
 
-				</div>
-				<div
-				onClick={()=>setSelectedColor("yellow")} 
-				className={"light yellow" + ((selectedColor === "yellow") ? " glow": "")}>
-
-				</div>
-				<div
-				onClick={()=>setSelectedColor("green")} 
-				className={"light green" + ((selectedColor === "green") ? " glow": "")}>
-
-				</div>
-
-			</div>
-		</>	
-	);
+  return (
+    <>
+      <div className="palito"></div>
+      <div className="semaforo">
+        <div
+          onClick={() => setSelectedColor("red")} // Corregir: cambiar "setSelectecColor" a "setSelectedColor"
+          className={"light red" + (selectedColor === "red" ? " glowred" : "")}
+        ></div>
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "light yellow" + (selectedColor === "yellow" ? " glowyellow" : "")
+          }
+        ></div>
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "light green" + (selectedColor === "green" ? " glowgreen" : "")
+          }
+        ></div>
+      </div>
+      <div className="Mensaje">⇈ Click on one ⇈</div>
+      <div className="Button">
+        <button class="Reload"onClick={() => window.location.reload()}>RELOAD</button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
